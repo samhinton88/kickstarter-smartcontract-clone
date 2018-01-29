758e6691df9e10054ea67e386965526152f53cd9
sam hinton <sam.hinton88@gmail.com>
2018-01-29 16:16:34 +0000

    grider ETH lct:142 deployed contract with deploy.js

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,6 +1,6 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
+const compiledFactory = require('./build/CampaignFactory.json')
 
 const provider = new HDWalletProvider(
   'put skill lumber start pair cigar broken large spider voice final history',
@@ -15,8 +15,8 @@ const deploy = async () => {
 
   console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
+  const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: compiledFactory.bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
