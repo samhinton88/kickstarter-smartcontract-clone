7f41b1294d1bced3e0c7fcd544ed62a8b68f5c7a
sam hinton <sam.hinton88@gmail.com>
2018-01-29 16:04:38 +0000

    grider ETH lct:141 copied deploy.js from lottery project

---
+++ ethereum/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'put skill lumber start pair cigar broken large spider voice final history',
+  'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q'
+);
+
+const web3 = new Web3(provider);
+
+// put deploy inside a function so that it can be marked async
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+
+deploy();
