83e3dab87baeb8c91845ea4aaa98dadc7fc88314
sam hinton <sam.hinton88@gmail.com>
2018-01-29 15:59:56 +0000

    grider ETH lct:140 added processes request test

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -87,5 +87,35 @@ describe('Campaigns', () => {
     const request = await campaign.methods.requests(0).call();
 
     assert.equal('Buy batteries', request.description);
+  });
+
+  it('processes requests', async () => {
+    await campaign.methods.contribute().send({
+      value: web3.utils.toWei('10', 'ether'),
+      from: accounts[0]
+    });
+
+    await campaign.methods
+      .createRequest('Buy batteries', web3.utils.toWei('5', 'ether'), accounts[1])
+      .send({
+        gas: '1000000',
+        from: accounts[0]
+      });
+
+    await campaign.methods.approveRequest(0).send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    await campaign.methods.finalizeRequest(0).send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    let balance = await web3.eth.getBalance(accounts[1]);
+    balance = web3.utils.fromWei(balance, 'ether');
+    balance = parseFloat(balance);
+    console.log(balance)
+    assert(balance > 103)
   })
 })
