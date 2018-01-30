0434abc097c41c1502d5b2b5e8ed0c953da0098f
sam hinton <sam.hinton88@gmail.com>
2018-01-30 15:07:22 +0000

    grider ETH lct:152 put logic into web3.js to account for code being run away from the browser (and therefore returning global window variable as undefined)

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,16 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+  // we are in the browser, metamask has already injected web3
+  web3 = new Web3(window.web3.currentProvider);
+} else {
+  // we are on the server or user is not running metamask
+  const provider = new Web3.providers.HttpProvider(
+    'https://rinkeby.infura.io/orDImgKRzwNrVCDrAk5Q'
+    );
+  web3 = new Web3(provider);
+}
 
 export default web3;
